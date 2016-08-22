35f3ac811612a841056bef76b7ebc3efd03b4310
phiresky <phireskyde+git@gmail.com>
2016-08-22 22:49:18 +0200

    add comments to config.ts

--- config.ts
+++ config.ts
@@ -1,8 +1,13 @@
 export const defaultConfig = {
     logFilesPath: "./logs/",
+    // scan backwards for 7 days before giving up when data is missing
     maxMissingDays: 7,
+    // only get this many days before stopping
     dayGetLimit: Infinity,
+    // must be set to the interval with which nmap is run in your crontab,
+    // e.g. 10 if your crontab entry is "*/10 * * * ..."
     logIntervalMinutes: 10,
+    // hide devices that are up less than 2% of the time
     minimumUptime: 0.02,
     selfMacAddress: "00:00:00:00:00:00",
     staticLogFiles: [] as string[],
